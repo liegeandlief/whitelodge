ccccf06505d47f78db7d3fbe33a44773fb4be2f9
Michael <liegeandlief@users.noreply.github.com>
2017-08-08 22:50:36 +0100

    Update maybe-publish.sh

--- maybe-publish.sh
+++ maybe-publish.sh
@@ -17,11 +17,7 @@ if [[ $newVersionLength -gt 0 && $newVersionFirstChar != "0" ]]; then
   if [[ $CI == true && $TRAVIS == true && $TRAVIS_BRANCH == "master" && $TRAVIS_PULL_REQUEST == false && $newVersion != $currentVersion ]]
   then
     greenEcho "Publishing to NPM"
-    npm login <<!
-    $NPM_USERNAME
-    $NPM_PASSWORD
-    $NPM_EMAIL
-    !
+    npm adduser
     npm publish
   fi
 fi
